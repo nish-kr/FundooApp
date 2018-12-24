8193bb0cba445d4c9a1156108b646299179f6d79
Nishant Kumar <nish.kr.94@gmail.com>
2018-12-24 18:46:49 +0530

    event emmiter & flex wrap

--- fundooClient/src/app/addnote/addnote.component.ts
+++ fundooClient/src/app/addnote/addnote.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit, Injectable, SimpleChanges, ViewChild } from '@angular/core';
 import { HttpService } from '../http.service';
-import { Router } from '@angular/router';
+import { Router, ChildActivationEnd } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
 import { NotesModel } from "../models/notes.model";
+import { log } from 'util';
+import { NotesComponent } from '../notes/notes.component';
 
 @Component({
   selector: 'app-addnote',
@@ -13,6 +15,8 @@ import { NotesModel } from "../models/notes.model";
 @Injectable()
 export class AddnoteComponent implements OnInit {
 
+  parentMessage: boolean;
+  noteData: any;
   rowCol: any;
   notes: NotesModel = new NotesModel();
   title: String;
@@ -22,11 +26,16 @@ export class AddnoteComponent implements OnInit {
     private httpService: HttpService,
     private router: Router,
     private snackBar: MatSnackBar
-  ) { }
+  ) {
+    // this.getNotes();
+  }
 
+  @ViewChild('child')
+  public child: NotesComponent;
   ngOnInit() {
     const isOpen = false;
-    // this.getNotes();
+    this.getNotes();
+    
   }
 
   addNote() {
@@ -34,7 +43,7 @@ export class AddnoteComponent implements OnInit {
     if (this.notes.note || this.notes.title) {
       let userCredentials = JSON.parse(localStorage.getItem("loginToken"));
 
-      var noteData = {
+      this.noteData = {
         title: this.notes.title,
         note: this.notes.note,
         reminder: "",
@@ -45,7 +54,7 @@ export class AddnoteComponent implements OnInit {
         userId: userCredentials.userId
       }
 
-      this.httpService.post(noteData, "addNote").subscribe(
+      this.httpService.post(this.noteData, "addNote").subscribe(
         data => {
           console.log("Data sent", data);
           // alert("Registration Successful");
@@ -60,29 +69,30 @@ export class AddnoteComponent implements OnInit {
       console.log(this.notes.title, " title ", this.notes.note);
       this.notes.title = null;
       this.notes.note = null;
-
+      this.getNotes();
     } else {
       console.log("Empty note!");
     }
   }
 
-  // getNotes() {
-  //   let userCredentials = JSON.parse(localStorage.getItem("loginToken"));
-  //   this.rowCol = "row";
-  //   var getNotesObj = {
-  //     userId: userCredentials.userId,
-  //     token: userCredentials.loginToken
-  //   }
+  getNotes() {
+    let userCredentials = JSON.parse(localStorage.getItem("loginToken"));
+    this.rowCol = "row";
+    var getNotesObj = {
+      userId: userCredentials.userId,
+      token: userCredentials.loginToken
+    }
 
-  //   this.httpService.post(getNotesObj, 'getNotes').subscribe(
-  //     data => {
-  //       this.data = data;
-  //       console.log(data);
-  //     },
-  //     error => {
-  //       console.log(error);
-  //     }
-  //   )
-  // }
+    this.httpService.post(getNotesObj, 'getNotes').subscribe(
+      data => {
+        this.data = data;
+        this.child.getNotes()
+      },
+      error => {
+        console.log(error);
+      }
+    )
+    return this.data;
+  }
 
 }
--- fundooClient/src/app/notes/notes.component.ts
+++ fundooClient/src/app/notes/notes.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { HttpService } from '../http.service';
-import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
-import { NotesModel } from "../models/notes.model";
 
 @Component({
   selector: 'app-notes',
@@ -12,58 +10,17 @@ import { NotesModel } from "../models/notes.model";
 
 export class NotesComponent implements OnInit {
 
-  notes: NotesModel = new NotesModel();
-  title: String;
-  note: String;
-  data:any;
-  rowCol:any;
+  // @Input() noteData: any;
+
+  data: any;
+  rowCol: any;
   constructor(
     private httpService: HttpService,
-    private router: Router,
     private snackBar: MatSnackBar
   ) { }
 
   ngOnInit() {
-    const isOpen = false;
-    this.getNotes();
-  }
-
-  addNote() {
-
-    if (this.notes.note || this.notes.title) {
-      let userCredentials = JSON.parse(localStorage.getItem("loginToken"));
-
-      var noteData = {
-        title: this.notes.title,
-        note: this.notes.note,
-        reminder: "",
-        pin: false,
-        trash: false,
-        archive: false,
-        color: "",
-        userId: userCredentials.userId
-      }
-
-      this.httpService.post(noteData, "addNote").subscribe(
-        data => {
-          console.log("Data sent", data);
-          // alert("Registration Successful");
-          this.snackBar.open("Note addition Successful!", "Okay!", { duration: 2000 })
-          // this.router.navigateByUrl('/login');
-        },
-        error => {
-          this.snackBar.open("Note addition Unsuccessful! Invalid Input(s) / Internal Error!", "Okay!", { duration: 2000 })
-          console.log("Internal HTTP Error: ", error);
-        }
-      )
-
-      console.log(this.notes.title, " title ", this.notes.note);
-      this.notes.title = null;
-      this.notes.note = null;
-
-    } else {
-      console.log("Empty note!");
-    }
+    // this.getNotes();
   }
 
   getNotes() {
@@ -73,11 +30,9 @@ export class NotesComponent implements OnInit {
       userId: userCredentials.userId,
       token: userCredentials.loginToken
     }
-
     this.httpService.post(getNotesObj, 'getNotes').subscribe(
       data => {
         this.data = data;
-        console.log(data);
       },
       error => {
         console.log(error);
